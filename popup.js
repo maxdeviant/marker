e4b9708ab870d7702eba836d892966148d930bb0
maxdeviant <mad.mod.max@gmail.com>
2013-12-09 12:11:16 -0500

    Successfully retrieve bookmarkTreeNodes structure.

--- popup.js
+++ popup.js
@@ -1,9 +1,16 @@
 var marker = {
-	list: function() {
-
+	list: function(query) {
+		$('#bookmarks').html('');
+		var bookmarkTreeNodes = chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
+			console.log(bookmarkTreeNodes);
+		});
 	}
 };
 
 $(document).ready(function() {
-	
+	marker.list($('#search').val());
+
+	$('#search').change(function() {
+		marker.list($('#search').val());
+	});
 });
