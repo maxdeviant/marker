cd24cb0fe4236ee49c34cff3a0219c030f1028f1
maxdeviant <mad.mod.max@gmail.com>
2013-12-09 11:50:23 -0500

    Add not necessary for scope of project.

--- popup.js
+++ popup.js
@@ -1,18 +1,9 @@
 var marker = {
-	add: function(title, url) {
-		chrome.bookmarks.create({
-			// 'parentId': bookmarkBar.id,
-			'title': title,
-			'url': url
-		});
-	},
 	list: function() {
 
 	}
 };
 
 $(document).ready(function() {
-	$('#add').click(function() {
-		marker.add($('#title').val(), $('#url').val());
-	});
+	
 });
