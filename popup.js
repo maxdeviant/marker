0e0c31ea3ff1170cdbbbfe2eddff4f460679fde9
maxdeviant <mad.mod.max@gmail.com>
2013-12-09 12:22:20 -0500

    Can properly traverse bookmark tree.

--- popup.js
+++ popup.js
@@ -1,16 +1,30 @@
-var marker = {
+var M = {
 	list: function(query) {
 		$('#bookmarks').html('');
 		var bookmarkTreeNodes = chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
-			console.log(bookmarkTreeNodes);
+			// console.log(bookmarkTreeNodes);
+			$('#bookmarks').append(M.processTree(bookmarkTreeNodes));
 		});
+	},
+	processTree: function(tree) {
+		console.log(tree);
+		for (var i = 0; i < tree.length; i++) {
+			if (tree[i].children) {
+				M.processTree(tree[i].children);
+			} else {
+				M.processNode(tree[i]);
+			}
+		}
+	},
+	processNode: function(node) {
+		console.log(node.url);
 	}
 };
 
 $(document).ready(function() {
-	marker.list($('#search').val());
+	M.list($('#search').val());
 
 	$('#search').change(function() {
-		marker.list($('#search').val());
+		M.list($('#search').val());
 	});
 });
